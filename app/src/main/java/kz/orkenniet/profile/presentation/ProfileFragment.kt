2b00ed172cf43473b7b04d12814f8aeef888b6b4
Aleke89 <alnur.rysbek@mail.ru>
2023-03-27 12:05:18 +0600

    (after fix)profile fragment

--- app/src/main/java/kz/orkenniet/profile/presentation/ProfileFragment.kt
+++ app/src/main/java/kz/orkenniet/profile/presentation/ProfileFragment.kt
@@ -1,19 +1,18 @@
 package kz.orkenniet.profile.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import kz.orkenniet.R
-import kz.orkenniet.databinding.FragmentLoginBinding
-import kz.orkenniet.databinding.FragmentProfilBinding
+import kz.orkenniet.databinding.FragmentProfileBinding
 
-class ProfilFragment : Fragment(R.layout.fragment_profil) {
-    private val binding: FragmentProfilBinding by viewBinding(FragmentProfilBinding::bind)
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private val binding: FragmentProfileBinding by viewBinding(FragmentProfileBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.root
     }
 }
