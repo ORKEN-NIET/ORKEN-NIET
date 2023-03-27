ce0781cf76e749681a9ca6488a5fa2832009d815
madiberikkazy004 <madiberikkazy004@gmail.com>
2023-03-27 16:35:02 +0600

    after fix

--- app/src/main/java/kz/orkenniet/library/presentation/LibraryFragment.kt
+++ app/src/main/java/kz/orkenniet/library/presentation/LibraryFragment.kt
@@ -1,15 +1,13 @@
 package kz.orkenniet.library.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import kz.orkenniet.R
 import kz.orkenniet.databinding.FragmentBibliotekaBinding
 
-class BibliotekaFragment : Fragment(R.layout.fragment_biblioteka) {
+class LibraryFragment : Fragment(R.layout.`fragment_library.xml`) {
 
     private val binding: FragmentBibliotekaBinding by viewBinding(FragmentBibliotekaBinding::bind)
 
