1dbde2eb812eede977ec35d14a67f1c61df7c774
Aleke89 <alnur.rysbek@mail.ru>
2023-03-28 14:21:20 +0600

    After fix

--- app/src/main/java/kz/orkenniet/admin/presentation/AdminMainActivity.kt
+++ app/src/main/java/kz/orkenniet/admin/presentation/AdminMainActivity.kt
@@ -17,7 +17,6 @@ class AdminMainActivity : AppCompatActivity(R.layout.activity_admin_main) {
 
     private val binding: ActivityAdminMainBinding by viewBinding(ActivityAdminMainBinding::bind)
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.navigation.setOnNavigationItemSelectedListener(navListener)
@@ -28,12 +27,12 @@ class AdminMainActivity : AppCompatActivity(R.layout.activity_admin_main) {
     }
 
     private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        var selectedFragment: Fragment? = null
-        when (item.itemId) {
-            R.id.glavnaya -> selectedFragment = HomeFragment()
-            R.id.biblioteka -> selectedFragment = LibraryFragment()
-            R.id.citaty -> selectedFragment = QuotesFragment()
-            R.id.profil -> selectedFragment = ProfileFragment()
+        var selectedFragment = when(item.itemId){
+            R.id.glavnaya -> HomeFragment()
+            R.id.biblioteka -> LibraryFragment()
+            R.id.citaty ->  QuotesFragment()
+            R.id.profil ->  ProfileFragment()
+            else -> null
         }
         supportFragmentManager.beginTransaction().replace(
             R.id.fragment_container,
