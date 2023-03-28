88069d76938fd485f14e6df5695d887f27166622
Aleke89 <alnur.rysbek@mail.ru>
2023-03-28 10:08:53 +0600

    After Fix SecondPageActivity and Rename FirstPage -> FirstPageActivity

--- app/src/main/java/kz/orkenniet/splash/SecondPageActivity.kt
+++ app/src/main/java/kz/orkenniet/splash/SecondPageActivity.kt
@@ -1,6 +1,5 @@
 package kz.orkenniet.splash
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,10 +7,19 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import kz.orkenniet.R
 import kz.orkenniet.databinding.ActivitySecondPageBinding
 
-class SecondPage : AppCompatActivity(R.layout.activity_second_page) {
+class SecondPageActivity : AppCompatActivity(R.layout.activity_second_page) {
 
     private val binding: ActivitySecondPageBinding by viewBinding(ActivitySecondPageBinding::bind)
-    var prevStarted = "yes"
+    private var prevStarted = "yes"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_page)
+        binding.imgNext.setOnClickListener {
+            onResume()
+        }
+    }
+
     override fun onResume() {
         super.onResume()
         val sharedpreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
@@ -26,16 +34,7 @@ class SecondPage : AppCompatActivity(R.layout.activity_second_page) {
         }
     }
 
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_page)
-        binding.imgNext.setOnClickListener {
-            onResume()
-        }
-    }
-
-    fun moveToSecondary() {
+    private fun moveToSecondary() {
         val intent = Intent(this, ThirdPage::class.java)
         startActivity(intent)
     }
