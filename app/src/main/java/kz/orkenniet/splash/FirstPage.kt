9a0a2f2b2e848809c20efdfa689ec06f510875aa
Aleke89 <alnur.rysbek@mail.ru>
2023-03-28 08:56:42 +0600

    FirstPage activity Java to Kotlin jasadym

---
+++ app/src/main/java/kz/orkenniet/splash/FirstPage.kt
@@ -0,0 +1,43 @@
+package kz.orkenniet.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import kz.orkenniet.R
+import kz.orkenniet.databinding.ActivityFirstPageBinding
+import java.lang.Boolean
+
+class FirstPage : AppCompatActivity(R.layout.activity_first_page) {
+
+    private val binding: ActivityFirstPageBinding by viewBinding(ActivityFirstPageBinding::bind)
+    private var prevStarted = "yes"
+
+    override fun onResume() {
+        super.onResume()
+        val sharedpreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
+        if (!sharedpreferences.getBoolean(prevStarted, false)) {
+            val editor = sharedpreferences.edit()
+            editor.putBoolean(prevStarted, Boolean.TRUE)
+            editor.apply()
+        } else {
+            if (binding.imgNext!!.isEnabled) {
+                moveToSecondary()
+            }
+        }
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.imgNext.setOnClickListener {
+            onResume()
+        }
+    }
+
+    fun moveToSecondary() {
+        val intent = Intent(this, SecondPage::class.java)
+        startActivity(intent)
+    }
+}
