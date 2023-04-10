99fc3898c882d986753bbf39be727bd89b02eb5f
Aleke89 <alnur.rysbek@mail.ru>
2023-04-10 10:37:11 +0600

    merging

--- app/src/main/java/kz/orkenniet/home/presentation/HomeFragment.kt
+++ app/src/main/java/kz/orkenniet/home/presentation/HomeFragment.kt
@@ -7,12 +7,28 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import kz.orkenniet.R
 import kz.orkenniet.databinding.FragmentHomeBinding
 
+
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val binding: FragmentHomeBinding by viewBinding(FragmentHomeBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.lGenre
+
+        binding.llAdventureGenre.setOnClickListener{
+            val transition = this.fragmentManager?.beginTransaction()
+                ?.replace(R.id.layout,SortedByAdventureGenreFragment())
+                ?.commit()
+        }
+        binding.llDetectiveGenre.setOnClickListener{
+            val transition = this.fragmentManager?.beginTransaction()
+                ?.replace(R.id.layout,SortedByDetectiveGenreFragment())
+                ?.commit()
+        }
+        binding.llFairyTailsGenre.setOnClickListener{
+            val transition = this.fragmentManager?.beginTransaction()
+                ?.replace(R.id.layout,SortedByTailsGenreFragment())
+                ?.commit()
+        }
     }
 }
