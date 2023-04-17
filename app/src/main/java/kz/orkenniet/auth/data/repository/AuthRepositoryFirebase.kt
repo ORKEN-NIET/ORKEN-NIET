e1e1556d44e8090e68cb57d052ec81ef4f447c40
Magzhan <m.sagatov@arbuz.kz>
2023-04-17 15:52:44 +0600

    fix check current user by clean architecture

--- app/src/main/java/kz/orkenniet/auth/data/repository/AuthRepositoryFirebase.kt
+++ app/src/main/java/kz/orkenniet/auth/data/repository/AuthRepositoryFirebase.kt
@@ -2,6 +2,7 @@ package kz.orkenniet.auth.data.repository
 
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
@@ -10,7 +11,8 @@ import kz.orkenniet.core.resource.Resource
 import kz.orkenniet.core.resource.safeCall
 
 class AuthRepositoryFirebase(
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseDatabase: FirebaseDatabase,
 ) : AuthRepository {
 
     override suspend fun login(email: String, password: String): Resource<AuthResult> {
@@ -29,5 +31,14 @@ class AuthRepositoryFirebase(
                 Resource.Success(result)
             }
         }
+    }
+
+    override fun isLoggedIn(): Boolean {
+        val databaseReference = firebaseDatabase.getReference("users")
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            databaseReference.child(currentUser.uid).child("isLoggedIn").setValue(true)
+        }
+        return currentUser != null
     }
 }
